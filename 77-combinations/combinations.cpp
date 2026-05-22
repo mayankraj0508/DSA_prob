f62ea4f21ff68ab185bede789423c9e7f43da4ed
mayankraj0508 <mrajdbg2018@gmail.com>
2026-05-22 17:21:52 +0530

    Time: 104 ms (15.76%) | Memory: 131.3 MB (13.97%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -4,20 +4,20 @@ public:
     void f(int sum ,int n,int k,vector<int>&temp){
            if(temp.size()==k){
             ans.push_back(temp);
-            // if(temp.size())
-            // temp.pop_back();
+            if(temp.size())
+            temp.pop_back();
             return ;
          }
         if(sum>n){
-            // if(temp.size())
-            // temp.pop_back();
+           if(temp.size())
+            temp.pop_back();
             return ;
         }
       
          temp.push_back(sum);
          f(sum+1,n,k,temp);
-         if(temp.size())
-         temp.pop_back();
+        //  if(temp.size())
+        //  temp.pop_back();
          f(sum+1,n,k,temp);   
     }
     vector<vector<int>> combine(int n, int k) {
