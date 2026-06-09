610d760b1054fd927b3c125b7228db34c763ef69
mayankraj0508 <mrajdbg2018@gmail.com>
2026-06-09 09:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (76.62%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int>m;
-        int i =0;
-        int j =0;
-        int ans =  1;
-        if(s.size()==1){
-            return 1;
+        if(s.size()==1 || s.size()==0){
+            return s.size();
         }
-        if(s.size()==0){
-            return 0;
-        }
-        bool flag = false;
-        while( j<s.size()){
-            m[s[j]]++;
-            if(m[s[j]]>=2){
-                flag = true;
-                 ans = max(ans,j-i);
-                while( i<s.size() && s[i]!=s[j]){
-                    
-                    m[s[i]]--;
-                    if(m[s[i]]==0){
-                        m.erase(s[i]);
-                    }
+        vector<int>freq(256,0);
+        int i =0; 
+        int j =1;
+        freq[s[i]]++;
+        int mx_size = 0;
+        while(i<s.size() && j<s.size()){
+            if(freq[s[j]]==0){
+                freq[s[j]]++;
+                j++;
+                
+                
+            }
+            else{
+                mx_size = max(mx_size,j-i);
+                while(i<s.size() && s[i]!=s[j]){
+                    freq[s[i]]--;
                     i++;
                 }
-                
-                m[s[j]]--;
+                freq[s[i]]--;
                 i++;
-          
+                freq[s[j]]++;
+                j++;
             }
-            j++;
-          
-            
+
         }
-        ans = max(ans,j-i);
-        return  ans;
+        mx_size = max(mx_size,j-i);
+        return mx_size;
+
+
         
     }
 };
