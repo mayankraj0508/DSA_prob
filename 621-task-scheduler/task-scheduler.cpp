99a0bde7055d0bb5b2b5af38155bc93b4bc41fad
mayankraj0508 <mrajdbg2018@gmail.com>
2026-06-22 08:59:00 +0530

    Time: 45 ms (17.11%) | Memory: 52.8 MB (8.00%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
     #define pp pair<int,char>
-
-    int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int> m;
-       for(int i =0; i<tasks.size(); i++){
-        m[tasks[i]]++;
-       }
-
-        priority_queue<pp> pq;
-        for(auto x : m){
+    int leastInterval(vector<char>& nums, int n) {
+        unordered_map<char,int>m;
+        for(int i =0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
+        priority_queue<pp>pq;
+        for(auto x:m){
             pq.push({x.second,x.first});
         }
-
         int time = 0;
-
-        while(!pq.empty()){
-            int prev_time = time;
-            int cycle = n + 1;
-
-            priority_queue<pp> temp;
-
-            while(cycle-- && !pq.empty()){
-                auto p = pq.top(); pq.pop();
-
-                int freq = p.first- 1;
-                time++;
-
-                if(freq > 0){
-                    temp.push({freq,p.second});
+        int prev_time = 0;
+        while(pq.size()){
+            prev_time = time;
+            vector<pp>temp;
+            while(time-prev_time<
+            n+1){
+                if(pq.size()==0 && temp.size()==0){
+                    return time;
                 }
-            }
+                if(pq.size()==0){
+                    time++;
+                }
+                else{
+                auto x   = pq.top();
+                int freq = x.first;
+                pq.pop();
+                freq--;
+                if(freq>0){
+                    temp.push_back({freq,x.second});
+                }
+                time++;
+                }
 
-            while(!temp.empty()){
-                pq.push(temp.top());
-                temp.pop();
             }
-
-            if(!pq.empty() && time-prev_time<n+1){
-                time = prev_time + (n + 1);
+            for(int i=0; i<temp.size(); i++){
+                pq.push(temp[i]);
             }
         }
-
+    
         return time;
+
+        
+              
     }
 };
