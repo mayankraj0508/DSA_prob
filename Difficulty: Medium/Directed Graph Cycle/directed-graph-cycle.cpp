1e92c1a8943f8467f7d575388e7fb1610c976e79
mayankraj0508 <mrajdbg2018@gmail.com>
2026-07-08 07:13:33 +0300

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,34 +1,47 @@
 class Solution {
   public:
+    vector<int>indegree;
     unordered_set<int>visited;
-    bool dfs(vector<vector<int>>&graph , int i,  int ref){
-        visited.insert(i);
-        for(int j = 0; j<graph[i].size(); j++){
-           if(graph[i][j]==ref){
-               return true;
-           }
-            if(visited.find(graph[i][j])==visited.end()&& dfs(graph,graph[i][j],ref)){
-                return true;
+    void bfs_topo(vector<vector<int>>&graph){
+        queue<int>q;
+        for(int i =0; i<indegree.size(); i++){
+            if(indegree[i]==0){
+                q.push(i);
+                visited.insert(i);
             }
         }
-        return false;
+        
+        while(q.size()){
+            int sz = q.size();
+            for(int i =0; i<sz; i++){
+                int x = q.front();
+                q.pop();
+                for(auto neighbour : graph[x]){
+                    indegree[neighbour]--;
+                    if(indegree[neighbour]==0){
+                        q.push(neighbour);
+                        visited.insert(neighbour);
+                    }
+                }
+            }
+        }
+        
     }
     bool isCyclic(int V, vector<vector<int>> &edges) {
-        // code here
+        indegree.resize(V,0);
         vector<vector<int>>graph(V);
         for(int i =0; i<edges.size(); i++){
             int u = edges[i][0];
             int v = edges[i][1];
             graph[u].push_back(v);
-        }
-        for(int i =0; i<V; i++){
-            visited.clear();
-                if(dfs(graph,i,i)){
-                    return true;
-                }
+            indegree[v]++;
             
         }
+        bfs_topo(graph);
+        if(visited.size()<V){
+            return true;
+        }
         return false;
-        
+      
     }
 };
