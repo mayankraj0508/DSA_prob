0833dc7395e38214fa310c316cf22e42e6f93650
mayankraj0508 <mrajdbg2018@gmail.com>
2026-07-27 08:27:28 +0300

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.90%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,17 +4,7 @@ public:
         int i = 0;
         int j = nums.size()-1;
         int last = 0;
-        if(nums.size()==0){
-            return 0;
-        }
-        if(nums.size()==1){
-              if(nums[0]==val){
-                return 0;
-              }
-              else{
-                return 1;
-              }
-        }
+      
         while(i<=j){
             if(nums[i]==val && nums[j]!=val){
                 swap(nums[i],nums[j]);
