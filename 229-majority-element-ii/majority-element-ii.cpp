69c9524318d46b72c24acbb946eac4256eedb89e
mayankraj0508 <mrajdbg2018@gmail.com>
2026-08-13 07:16:10 +0300

    Time: 0 ms (100.00%) | Memory: 23.7 MB (54.67%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,20 +1,51 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& arr) {
+    vector<int> majorityElement(vector<int>& nums) {
+        long long  candidate1 = LLONG_MIN;
+        long long  candidate2  = LLONG_MIN;
+        int freq1 = 0;
+        int freq2 = 0;
+        for(int i = 0; i<nums.size(); i++){
+           if(freq1==0 && nums[i]!=candidate2){
+               candidate1 = nums[i];
+               freq1 = 1;
+           }
+           else if(freq2==0 && nums[i]!=candidate1){
+            candidate2  = nums[i];
+            freq2 = 1;
+           }
+           else if(nums[i]==candidate1){
+              freq1++;
+           }
+           else if(nums[i]==candidate2){
+            freq2++;
+           }
+           else {
+               freq1--;
+               freq2--;
+              
+
+           }
+            
+        }
         vector<int>ans;
-        int n  = arr.size();
-        unordered_map<int,int>m;
-        for(int i =0; i<n; i++){
-            m[arr[i]]++;
+           int count1 = 0;
+        int count2 = 0;
+       
+        for (int x : nums) {
+            if (x == candidate1)
+                count1++;
+            else if (x == candidate2)
+                count2++;
         }
-        int freq = n/3;
-        for(auto x:m){
-            if(x.second>freq){
-                ans.push_back(x.first);
-            }
+        int n = nums.size();
+        if(count1>n/3){
+            ans.push_back(candidate1);
         }
-        return ans;
-        
+        if(count2>n/3){
+            ans.push_back(candidate2);
+        }
+        return ans ;
         
     }
 };
