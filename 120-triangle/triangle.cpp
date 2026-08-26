e86662d9c81f7837f00cb243e9b93564cb92551e
mayankraj0508 <mrajdbg2018@gmail.com>
2026-08-26 18:07:59 +0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (92.88%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,15 +1,32 @@
+// class Solution {
+// public:
+//      vector<vector<int>>dp;
+//      int f(int i, int j,vector<vector<int>>&triangle){
+//         if(i>=triangle.size()) return 0;
+//         if(j>=triangle[i].size()) return INT_MAX;
+//          if(dp[i][j]!=INT_MAX) return dp[i][j];
+//         return dp[i][j] =  min(triangle[i][j]+f(i+1,j,triangle),triangle[i][j]+f(i+1,j+1,triangle));
+//      }
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         dp.resize(300,vector<int>(300,INT_MAX));
+//         return f(0,0,triangle);
+        
+//     }
+// };
 class Solution {
 public:
-     vector<vector<int>>dp;
-     int f(int i, int j,vector<vector<int>>&triangle){
-        if(i>=triangle.size()) return 0;
-        if(j>=triangle[i].size()) return INT_MAX;
-         if(dp[i][j]!=INT_MAX) return dp[i][j];
-        return dp[i][j] =  min(triangle[i][j]+f(i+1,j,triangle),triangle[i][j]+f(i+1,j+1,triangle));
-     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        dp.resize(300,vector<int>(300,INT_MAX));
-        return f(0,0,triangle);
-        
+
+        for(int i = triangle.size() - 2; i >= 0; i--) {
+            for(int j =triangle[i].size()-1; j >=0; j--) {
+
+                triangle[i][j] += min(
+                    triangle[i+1][j],
+                    triangle[i+1][j+1]
+                );
+            }
+        }
+
+        return triangle[0][0];
     }
 };
