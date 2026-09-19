5a5db083c756dcb4124c7e7c1d1b4ed11556b848
mayankraj0508 <mrajdbg2018@gmail.com>
2026-09-19 11:35:11 +0530

    Time: 10 ms (16.87%) | Memory: 30.5 MB (5.12%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,37 +4,26 @@ public:
         int n = nums.size();
         vector<int>nge(n);
         stack<int>st;
-        int idx = -1;
-        for(int i = 0; i<n-1; i++ ){
-            if(nums[i]>nums[n-1]){
-                nge[n-1] = i;
-                idx = i;
-                break;
-            }
-        }
-        if(idx==-1) nge[n-1] = -1;
-       
         for(int i = n-2; i>=0; i--){
             st.push(i);
         }
-        st.push(n-1);
-        for(int i = n-2; i>=0; i--){
+        for(int i = n-1; i>=0; i--){
             while(st.size() && nums[i]>=nums[st.top()]){
                 st.pop();
             }
-            if(st.size()==0) nge[i] = -1;
-            else 
-            nge[i] =st.top();
+            if(st.size()) nge[i] = st.top();
+            else nge[i] = -1;
             st.push(i);
         }
         vector<int>ans;
-        for(int i = 0; i<nge.size(); i++){
-            if(nge[i]==-1) {
+        for(int i = 0;i<nums.size(); i++){
+            if(nge[i]==-1){
                 ans.push_back(-1);
                 continue;
             }
-           ans.push_back(nums[nge[i]]);
+            ans.push_back(nums[nge[i]]);
         }
         return ans ;
+        
     }
 };
