a41591db6e6d639a0df7247843014a7738c65110
mayankraj0508 <mrajdbg2018@gmail.com>
2026-09-19 13:04:48 +0530

    Time: 8 ms (95.57%) | Memory: 47.9 MB (88.20%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,138 +1,183 @@
-#define pp pair<int, int>
-class Node {
-public:
-    Node* prev;
-    Node* next;
-    pp val;
-    Node(pp val) {
-        this->val = val;
+class Node{
+    public :
+    Node*next;
+    Node*prev;
+    int val;
+    int key;
+    Node(int key, int val){
+        this->val  = val;
+        this->key  = key;
         next = NULL;
-        prev = NULL;
+        prev  = NULL;
+
     }
 };
+
+
+
+
 class LRUCache {
 public:
-    Node* head;
-    Node* tail;
-    Node* prev;
-    int size;
-    int k;
-    unordered_map<int, Node*> m;
+    Node*head;
+    Node*tail;
+    int c;
+    int capacity  ;
+    unordered_map<int,Node*>m;
     LRUCache(int capacity) {
-        head = NULL;
-        prev = NULL;
-        tail = NULL;
-        size = 0;
-        k = capacity;
+        head  = NULL;
+        tail  = NULL;
+        c  = 0;
+        this->capacity  = capacity;
+        
     }
-
+    // tail ke next pe most recentlyvused 
     int get(int key) {
-        if (m.find(key) == m.end()) {
-            return -1;
+        if(m.find(key)!=m.end()){
+            int x = m[key]->val;
+           Node*temp = m[key];
+          if(temp==tail){
+               return x;
+           }
+           if(temp==head){
+               if(head){
+               head  = head->next;
+               if(head)
+               head->prev  = NULL;
+           }
+               if(tail)
+               tail->next  = temp;
+               temp->prev  = tail;
+               tail  = temp;
+               tail->next  = NULL;
+              
+               
+           }
+           
+           else{
+           Node*c = temp->prev;
+           Node*d = temp->next;
+           if(c)
+           c->next= d;
+           if(d)
+           d->prev  = c;
+           if(tail)
+           tail->next  = temp;
+           temp->prev  = tail;
+           tail  = temp;
+           tail->next  = NULL;
         }
-        if (size == 0) {
-            return -1;
-        }
-        if (head == tail) {
-            return head->val.second;
-        }
-        Node* c = m[key];
-        if (c == head) {
-            return head->val.second;
-        }
-        Node* t = c->next;
-        Node* p = c->prev;
-        bool flag = false;
-        if (p)
-            p->next = t;
-        if (t == NULL) {
-            Node* l = tail->prev;
-            Node* k = tail;
-            l->next = NULL;
-            tail = l;
-            k->next = head;
-            head->prev = k;
-            head = k;
-            k->prev = NULL;
-            flag = true;
-        }
-        if (t)
-            t->prev = p;
-        if (flag == false) {
-            c->next = head;
-            head->prev = c;
-            head = c;
-            c->prev = NULL;
-        }
+           return x;
 
-        int ans = head->val.second;
-        return ans;
+
+
+
+        }
+        
+        return -1;
     }
-
+    
     void put(int key, int value) {
-        if (m.find(key) != m.end()) {
-            Node* p = m[key];
-            if (p == head) {
-                head->val.second = value;
-                m[key]->val.second = value;
-                return;
-            }
-            if (head == tail) {
-                head->val.second = value;
-                m[key]->val.second = value;
-                return;
-            }
+        if(m.find(key)!=m.end()){
+            
+            m[key]->val = value;
+            Node*temp = m[key];
+            if(head==tail) return ;
 
-            if (p == tail) {
-                Node* k = tail->prev;
-                Node* j = tail;
-                k->next = NULL;
-                tail = k;
-                j->next = head;
-                head->prev = j;
-                j->prev = NULL;
-                head = j;
-                m[key]->val.second = value;
-                return;
-
-            } else {
-                Node* a = p->prev;
-                Node* b = p->next;
-                if (a)
-                    a->next = b;
-                if (b)
-                    b->prev = a;
-                p->next = head;
-                head->prev = p;
-                p->prev = NULL;
-                head = p;
-                m[key]->val.second = value;
-                return;
-            }
-        } else {
-            Node* p = new Node({key, value});
-            m[key] = p;
-            if (head == NULL) {
-                head = p;
-                tail = p;
-                size++;
-
-            } else {
-                p->next = head;
-                head->prev = p;
-                p->prev = NULL;
-                head = p;
-                size++;
-            }
-            if (size > k) {
-                // deletion
-                int valu = tail->val.first;
-                tail = tail->prev;
-                tail->next = NULL;
-                size--;
-                m.erase(valu);
-            }
+             if(temp==head){
+                if(head){
+               head  = head->next;
+               head->prev  = NULL;
+             }
+               
+               if(tail)
+               tail->next  = temp;
+               temp->prev  = tail;
+               tail  = temp;
+               tail->next  = NULL;
+               m[key] = tail;
+               
+           }
+           else if(temp==tail){
+               return ;
+           }
+           else{
+           Node*c = temp->prev;
+           Node*d = temp->next;
+           if(c)
+           c->next= d;
+           if(d)
+           d->prev  = c;
+           if(tail)
+           tail->next  = temp;
+           temp->prev  = tail;
+           tail  = temp;
+           tail->next  = NULL;
+           m[key] = tail;
         }
+
+        }
+        else {
+            if(c<capacity){
+               
+                Node*temp = new Node(key,value);
+                if(head==NULL){
+                    head = temp;
+                    tail  = temp;
+                    m[key] = head;
+                    c++;
+                    return ;
+                }
+                if(head==tail){
+                    tail->next  = temp;
+                    temp->prev = tail;
+                    tail  = temp;
+                    tail->next  = NULL;
+                    c++;
+                    m[key] = tail;
+                    return ;
+
+                }
+                if(tail)
+                tail->next  = temp;
+                temp->prev  = tail;
+                tail  = temp;
+                tail->next  = NULL;
+                m[key] = tail;
+                c++;
+
+
+            }
+            else{
+                int x  = -1;
+                if(head==tail){
+                    int y = head->key;
+                    Node*temp = new Node(key,value);
+                    head  = temp;
+                    tail  = temp;
+                    m[key] = head;
+                    m.erase(y);
+                    return  ;
+                }
+                if(head)
+                 x = head->key;
+                if(head)
+                head = head->next;
+
+                Node*temp = new Node(key,value);
+                if(tail)
+                tail->next  = temp;
+                temp->prev  = tail;
+                tail  = temp;
+                tail->next  = NULL;
+                m[key] = temp;
+                if(x!=-1)
+                m.erase(x);
+
+            }
+
+
+        }
+        
     }
 };
 
